0d2d66b979cabbf80263e8d84a9682362099298e
Benson, Clay <Clay.Benson@ellucian.com>
2018-04-11 13:37:13 -0400

    Initial code test

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const hapi = require('hapi');
+require('dotenv').config();
+
+// Create a server with a host and port
+const server = hapi.server({
+    host: 'localhost',
+    port: 5337
+});
+
+// Add the route
+server.route({
+    method: 'POST',
+    path: '/slackslash',
+    handler: function (request, h) {
+
+        return h.response('yeet').type('text/html').code(200);
+    }
+});
+
+// Start the server
+async function start() {
+
+    try {
+        await server.start();
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+};
+
+start();
