55a5c4a3a48ec8b79cd0b878d98e11386e516955
ClayBenson94 <contact@claybenson.me>
2018-04-15 21:43:44 -0400

    Way (debatable) better stats reporting

--- src/commands/roshambo.js
+++ src/commands/roshambo.js
@@ -32,74 +32,46 @@ async function roshamboGame (request, h, move, currentPlayer, targetPlayer) {
         // Remove the game from roshambo_games
         await postgres.query('DELETE FROM roshambo_games WHERE initiating_player = $1 AND target_player = $2;', [targetPlayer, currentPlayer]);
 
-        // Add the appropriate stats rows if necessary
-        const currentPlayerStatsResponse = await postgres.query('SELECT FROM roshambo_stats WHERE player = $1;', [currentPlayer]);
-        if (currentPlayerStatsResponse.rows.length === 0) {
-            await postgres.query('INSERT INTO roshambo_stats(player) VALUES($1) RETURNING *;', [currentPlayer]);
-        }
-        const targetPlayerStatsResponse = await postgres.query('SELECT FROM roshambo_stats WHERE player = $1;', [targetPlayer]);
-        if (targetPlayerStatsResponse.rows.length === 0) {
-            await postgres.query('INSERT INTO roshambo_stats(player) VALUES($1) RETURNING *;', [targetPlayer]);
-        }
-
         switch (gameToRespondTo.initial_move) {
         case 'rock':
-            await postgres.query('UPDATE roshambo_stats SET num_rocks_played = num_rocks_played + 1 WHERE player = $1;', [gameToRespondTo.initiating_player]);
             if (move === 'rock') {
-                await postgres.query('UPDATE roshambo_stats SET ties = ties + 1 WHERE player = $1 OR player = $2;', [gameToRespondTo.initiating_player, gameToRespondTo.target_player]);
-                await postgres.query('UPDATE roshambo_stats SET num_rocks_played = num_rocks_played + 1 WHERE player = $1;', [gameToRespondTo.target_player]);
+                await postgres.query('INSERT INTO roshambo_stats(initiating_player, target_player, initial_move, target_move, winner) VALUES($1, $2, $3, $4, $5);', [gameToRespondTo.initiating_player, gameToRespondTo.target_player, gameToRespondTo.initial_move, move, null]);
                 return respond(request, h, `Your rocks clash together, resulting in a tie between <@${gameToRespondTo.initiating_player}> and <@${gameToRespondTo.target_player}>!`, true);
             }
             if (move === 'paper') {
-                await postgres.query('UPDATE roshambo_stats SET wins = wins + 1 WHERE player = $1;', [gameToRespondTo.target_player]);
-                await postgres.query('UPDATE roshambo_stats SET losses = losses + 1 WHERE player = $1;', [gameToRespondTo.initiating_player]);
-                await postgres.query('UPDATE roshambo_stats SET num_papers_played = num_papers_played + 1 WHERE player = $1;', [gameToRespondTo.target_player]);
+                await postgres.query('INSERT INTO roshambo_stats(initiating_player, target_player, initial_move, target_move, winner) VALUES($1, $2, $3, $4, $5);', [gameToRespondTo.initiating_player, gameToRespondTo.target_player, gameToRespondTo.initial_move, move, gameToRespondTo.target_player]);
                 return respond(request, h, `<@${gameToRespondTo.initiating_player}>'s rock gets covered by <@${gameToRespondTo.target_player}>'s paper, which somehow is a victory!`, true);
             }
             if (move === 'scissors') {
-                await postgres.query('UPDATE roshambo_stats SET wins = wins + 1 WHERE player = $1;', [gameToRespondTo.initiating_player]);
-                await postgres.query('UPDATE roshambo_stats SET losses = losses + 1 WHERE player = $1;', [gameToRespondTo.target_player]);
-                await postgres.query('UPDATE roshambo_stats SET num_scissors_played = num_scissors_played + 1 WHERE player = $1;', [gameToRespondTo.target_player]);
+                await postgres.query('INSERT INTO roshambo_stats(initiating_player, target_player, initial_move, target_move, winner) VALUES($1, $2, $3, $4, $5);', [gameToRespondTo.initiating_player, gameToRespondTo.target_player, gameToRespondTo.initial_move, move, gameToRespondTo.initiating_player]);
                 return respond(request, h, `<@${gameToRespondTo.initiating_player}>'s rock smashes <@${gameToRespondTo.target_player}>'s scissors to smithereens!`, true);
             }
             break;
         case 'paper':
-            await postgres.query('UPDATE roshambo_stats SET num_papers_played = num_papers_played + 1 WHERE player = $1;', [gameToRespondTo.initiating_player]);
             if (move === 'rock') {
-                await postgres.query('UPDATE roshambo_stats SET wins = wins + 1 WHERE player = $1;', [gameToRespondTo.initiating_player]);
-                await postgres.query('UPDATE roshambo_stats SET losses = losses + 1 WHERE player = $1;', [gameToRespondTo.target_player]);
-                await postgres.query('UPDATE roshambo_stats SET num_rocks_played = num_rocks_played + 1 WHERE player = $1;', [gameToRespondTo.target_player]);
+                await postgres.query('INSERT INTO roshambo_stats(initiating_player, target_player, initial_move, target_move, winner) VALUES($1, $2, $3, $4, $5);', [gameToRespondTo.initiating_player, gameToRespondTo.target_player, gameToRespondTo.initial_move, move, gameToRespondTo.initiating_player]);
                 return respond(request, h, `<@${gameToRespondTo.initiating_player}>'s paper covers <@${gameToRespondTo.target_player}>'s rock, which somehow is a victory!`, true);
             }
             if (move === 'paper') {
-                await postgres.query('UPDATE roshambo_stats SET ties = ties + 1 WHERE player = $1 OR player = $2;', [gameToRespondTo.initiating_player, gameToRespondTo.target_player]);
-                await postgres.query('UPDATE roshambo_stats SET num_papers_played = num_papers_played + 1 WHERE player = $1;', [gameToRespondTo.target_player]);
+                await postgres.query('INSERT INTO roshambo_stats(initiating_player, target_player, initial_move, target_move, winner) VALUES($1, $2, $3, $4, $5);', [gameToRespondTo.initiating_player, gameToRespondTo.target_player, gameToRespondTo.initial_move, move, null]);
                 return respond(request, h, `Your papers flutter in the wind, resulting in a tie between <@${gameToRespondTo.initiating_player}> and <@${gameToRespondTo.target_player}>!`, true);
             }
             if (move === 'scissors') {
-                await postgres.query('UPDATE roshambo_stats SET wins = wins + 1 WHERE player = $1;', [gameToRespondTo.target_player]);
-                await postgres.query('UPDATE roshambo_stats SET losses = losses + 1 WHERE player = $1;', [gameToRespondTo.initiating_player]);
-                await postgres.query('UPDATE roshambo_stats SET num_scissors_played = num_scissors_played + 1 WHERE player = $1;', [gameToRespondTo.target_player]);
+                await postgres.query('INSERT INTO roshambo_stats(initiating_player, target_player, initial_move, target_move, winner) VALUES($1, $2, $3, $4, $5);', [gameToRespondTo.initiating_player, gameToRespondTo.target_player, gameToRespondTo.initial_move, move, gameToRespondTo.target_player]);
                 return respond(request, h, `<@${gameToRespondTo.initiating_player}>'s paper is sliced into oragami by <@${gameToRespondTo.target_player}>'s scissors!`, true);
             }
             break;
         case 'scissors':
-            await postgres.query('UPDATE roshambo_stats SET num_scissors_played = num_scissors_played + 1 WHERE player = $1;', [gameToRespondTo.initiating_player]);
             if (move === 'rock') {
-                await postgres.query('UPDATE roshambo_stats SET wins = wins + 1 WHERE player = $1;', [gameToRespondTo.target_player]);
-                await postgres.query('UPDATE roshambo_stats SET losses = losses + 1 WHERE player = $1;', [gameToRespondTo.initiating_player]);
-                await postgres.query('UPDATE roshambo_stats SET num_rocks_played = num_rocks_played + 1 WHERE player = $1;', [gameToRespondTo.target_player]);
+                await postgres.query('INSERT INTO roshambo_stats(initiating_player, target_player, initial_move, target_move, winner) VALUES($1, $2, $3, $4, $5);', [gameToRespondTo.initiating_player, gameToRespondTo.target_player, gameToRespondTo.initial_move, move, gameToRespondTo.target_player]);
                 return respond(request, h, `<@${gameToRespondTo.initiating_player}>'s scissors get smashed to smithereens by <@${gameToRespondTo.target_player}>'s rock!`, true);
             }
             if (move === 'paper') {
-                await postgres.query('UPDATE roshambo_stats SET wins = wins + 1 WHERE player = $1;', [gameToRespondTo.initiating_player]);
-                await postgres.query('UPDATE roshambo_stats SET losses = losses + 1 WHERE player = $1;', [gameToRespondTo.target_player]);
-                await postgres.query('UPDATE roshambo_stats SET num_papers_played = num_papers_played + 1 WHERE player = $1;', [gameToRespondTo.target_player]);
+                await postgres.query('INSERT INTO roshambo_stats(initiating_player, target_player, initial_move, target_move, winner) VALUES($1, $2, $3, $4, $5);', [gameToRespondTo.initiating_player, gameToRespondTo.target_player, gameToRespondTo.initial_move, move, gameToRespondTo.initiating_player]);
                 return respond(request, h, `<@${gameToRespondTo.initiating_player}>'s scissors slice <@${gameToRespondTo.target_player}> paper into oragami!`, true);
             }
             if (move === 'scissors') {
-                await postgres.query('UPDATE roshambo_stats SET ties = ties + 1 WHERE player = $1 OR player = $2;', [gameToRespondTo.initiating_player, gameToRespondTo.target_player]);
-                await postgres.query('UPDATE roshambo_stats SET num_scissors_played = num_scissors_played + 1 WHERE player = $1;', [gameToRespondTo.target_player]);
+                await postgres.query('INSERT INTO roshambo_stats(initiating_player, target_player, initial_move, target_move, winner) VALUES($1, $2, $3, $4, $5);', [gameToRespondTo.initiating_player, gameToRespondTo.target_player, gameToRespondTo.initial_move, move, null]);
                 return respond(request, h, `Your scissors clang together like swords in battle, resulting in a tie between <@${gameToRespondTo.initiating_player}> and <@${gameToRespondTo.target_player}>!`, true);
             }
             break;
@@ -118,7 +90,14 @@ async function roshamboGame (request, h, move, currentPlayer, targetPlayer) {
  * @param {*} userId The id of the player to get stats on
  */
 async function roshamboStats (request, h, userId) {
-    const statsResponse = await postgres.query('SELECT * FROM roshambo_stats WHERE player = $1;', [userId]);
+    const statsResponse = await postgres.query(`
+        SELECT (SELECT COUNT(*) FROM roshambo_stats WHERE (initiating_player = $1 OR target_player = $1) AND (winner = $1)) AS wins,
+        (SELECT COUNT(*) FROM roshambo_stats WHERE (initiating_player = $1 OR target_player = $1) AND (winner is NULL)) AS ties,
+        (SELECT COUNT(*) FROM roshambo_stats WHERE (initiating_player = $1 OR target_player = $1) AND (winner != $1)) AS losses,
+        (SELECT COUNT(*) FROM roshambo_stats WHERE (initiating_player = $1 AND initial_move = 'rock') OR (target_player = $1 AND target_move = 'rock')) AS rocks,
+        (SELECT COUNT(*) FROM roshambo_stats WHERE (initiating_player = $1 AND initial_move = 'paper') OR (target_player = $1 AND target_move = 'paper')) AS papers,
+        (SELECT COUNT(*) FROM roshambo_stats WHERE (initiating_player = $1 AND initial_move = 'scissors') OR (target_player = $1 AND target_move = 'scissors')) AS scissors;
+    `, [userId]);
     if (statsResponse.rows[0]) {
         const stats = statsResponse.rows[0];
         return respondAttachments(request, h, [
@@ -133,8 +112,8 @@ async function roshamboStats (request, h, userId) {
                         short: false
                     },
                     {
-                        title: 'Rocks / Papers / Scissors played',
-                        value: `${stats.num_rocks_played} / ${stats.num_papers_played} / ${stats.num_scissors_played}`,
+                        title: 'Rocks / Papers / Scissors',
+                        value: `${stats.rocks} / ${stats.papers} / ${stats.scissors}`,
                         short: false
                     }
                 ],
