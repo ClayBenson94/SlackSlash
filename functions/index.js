20e2c932c3372663efc1de0d407a4f11202d01b3
ClayBenson94 <contact@claybenson.me>
2020-02-29 17:17:32 -0500

    Initial firebase functions work

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,55 @@
 const functions = require('firebase-functions');
 const axios = require('axios');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.getIP = functions.https.onRequest(async (request, response) => {
-	try {
-		const myIP = await axios.get('https://icanhazip.com');
-		response.send(myIP.data.trim());
-	} catch (e) {
-		console.error('Unhandled error in getIP', e);
-		response.status(500).send();
+const app = express();
+
+async function respond ({responseURL, text, respondInChannel}) {
+	await axios({
+		method: 'post',
+		url: responseURL,
+		data: {
+			response_type: respondInChannel ? 'in_channel' : 'ephemeral',
+			text: text
+		}
+	});
+}
+
+app.use((req, res, next) => {
+	if (!req.body) {
+		return res.status(400).json({ msg: 'No slack payload was provided' });
 	}
+
+	if (req.body.token !== functions.config().slackslash.slack_verification_token) {
+		return res.status(401).json({ msg: 'Invalid slack verification token' });
+	}
+
+	return next();
 });
+
+app.use('/catfact', async (req, res) => {
+	let catFactResponse;
+	try {
+		catFactResponse = await axios({
+			method: 'GET',
+			url: 'https://cat-fact.herokuapp.com/facts/random'
+		});
+	} catch (e) {
+		console.error('Error calling to get cat fact', e);
+		await respond({
+			responseURL: req.body.response_url,
+			text: `Failed to retrieve cat fact from cat-fact.herokuapp.com. Perhaps the app is down?`,
+			respondInChannel: false
+		});
+		return res.status(200).end();
+	}
+	const factText = catFactResponse.data.text;
+	await respond({
+		responseURL: req.body.response_url,
+		text: `<@${req.body.user_id}>'s cat fact:\n${factText}`,
+		respondInChannel: true
+	});
+	return res.status(200).end();
+});
+
+exports.main = functions.https.onRequest(app);
--- src/utilities/responseUtilities.js
+++ src/utilities/responseUtilities.js
@@ -25,32 +25,3 @@ async function respond (payload, h, text, respondInChannel) {
 	}
 	return h.response().code(200);
 }
-
-/**
- * Responds to the slack `response_url` with the given text. Optionally can respond in channel or ephemerally
- * @param {*} payload The slack payload object
- * @param {*} h The hapi response toolkit
- * @param {*} attachments The attachments object
- * @param {*} respondInChannel Whether or not to respond in channel (or ephemeral)
- */
-async function respondAttachments (payload, h, attachments, respondInChannel) {
-	const url = payload.response_url;
-	try {
-		await axios({
-			method: 'post',
-			url: url,
-			data: {
-				response_type: respondInChannel ? 'in_channel' : 'ephemeral',
-				attachments: attachments
-			}
-		});
-	} catch (e) {
-		return h.response().code(500);
-	}
-	return h.response().code(200);
-}
-
-module.exports = {
-	respond,
-	respondAttachments
-};
