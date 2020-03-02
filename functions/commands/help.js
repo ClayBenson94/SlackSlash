b56fc1d52d718869e189ea271a5eb1e508a403cb
ClayBenson94 <contact@claybenson.me>
2020-03-01 22:48:55 -0500

    Update help text

--- functions/commands/help.js
+++ functions/commands/help.js
@@ -3,7 +3,7 @@ const slackRespond = require('../utilities').slackRespond;
 async function help (req, res) {
 	await slackRespond({
 		responseURL: req.body.response_url,
-		text: 'To see more about the commands I support, click my picture in the chat (to the left) and click "About this app"!',
+		text: 'To see more about the commands I support, click my picture in the chat to the left and click "About this app"!',
 		respondInChannel: false
 	});
 	return res.status(200).end();
