226c45858e90a7421c35be6bbdd18773ea9d50e5
ClayBenson94 <contact@claybenson.me>
2020-08-08 22:35:00 -0400

    Using module again

--- functions/commands/catfact.js
+++ functions/commands/catfact.js
@@ -1,29 +1,23 @@
-const slackRespond = require('../utilities').slackRespond;
 const axios = require('axios');
 
-const catFact = async (req, res) => {
-	let catFactResponse;
+const catFact = async ({command, ack, respond}) => {
+	await ack();
+
 	try {
-		catFactResponse = await axios({
+		const catFactResponse = await axios({
 			method: 'GET',
-			url: 'https://cat-fact.herokuapp.com/facts/random'
+			url: 'https://cat-fact.herokuapp.com/facts/random',
 		});
+
+		const factText = catFactResponse.data.text;
+		await respond(`<@${command.user_id}>'s cat fact:\n${factText}`)
 	} catch (e) {
-		console.error('Error calling to get cat fact', e);
-		await slackRespond({
-			responseURL: req.body.response_url,
-			text: `Failed to retrieve cat fact from cat-fact.herokuapp.com. Perhaps the app is down?`,
-			respondInChannel: false
+		console.error('Error to getting cat fact', e);
+		await respond({
+			response_type: 'ephemeral',
+			text: 'Failed to retrieve cat fact from cat-fact.herokuapp.com. Perhaps the app is down?',
 		});
-		return res.status(200).end();
 	}
-	const factText = catFactResponse.data.text;
-	await slackRespond({
-		responseURL: req.body.response_url,
-		text: `<@${req.body.user_id}>'s cat fact:\n${factText}`,
-		respondInChannel: true
-	});
-	return res.status(200).end();
 }
 
 module.exports = catFact;
--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const { App, ExpressReceiver } = require('@slack/bolt');
 const functions = require('firebase-functions');
+const axios = require('axios');
 
 // Using an ExpressReceiver since Firebase Functions expect an express app in the export.
 const expressReceiver = new ExpressReceiver({
@@ -17,10 +18,6 @@ const app = new App({
 app.error(console.log);
 
 /* Add functionality here */
-app.command('/catfact', async ({command, ack, respond}) => {
-	console.log('WE IN THE CAT FACT');
-	await ack();
-	await respond(`You said "${command.text}"`);
-});
+app.command('/catfact', require('./commands/catfact');
 
 exports.slackslash = functions.https.onRequest(expressReceiver.app);
