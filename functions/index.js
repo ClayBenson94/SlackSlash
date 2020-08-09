c17bd1db5b379eb7dee3ae392a71f2eeb340e41c
ClayBenson94 <contact@claybenson.me>
2020-08-08 21:24:33 -0400

    bolt-js initial rework

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,23 @@
+const { App } = require('@slack/bolt');
 const functions = require('firebase-functions');
-const express = require('express');
 
-// Express app configuration
-const app = express();
-// Validate all requests through a middlware that checks the validation token
-app.use((req, res, next) => {
-	if (!req.body) {
-		return res.status(400).json({ msg: 'No slack payload was provided' });
-	}
-
-	if (req.body.token !== functions.config().slackslash.slack_verification_token) {
-		return res.status(401).json({ msg: 'Invalid slack verification token' });
-	}
-
-	return next();
+const app = new App({
+	signingSecret: functions.config().slackslash.slack_signing_secret,
+	token: functions.config().slackslash.slack_verification_token,
 });
 
-app.use('/cat-fact', require('./commands/catfact'));
-app.use('/help', require('./commands/help'));
+/* Add functionality here */
+
+(async () => {
+  // Start the app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
+
+app.command('cat-fact', async ({say, respond, ack}) => {
+	await ack();
+	await respond('Testing a cat fact. This is not a real cat fact.');
+});
 
 exports.slackslash = functions.https.onRequest(app);
